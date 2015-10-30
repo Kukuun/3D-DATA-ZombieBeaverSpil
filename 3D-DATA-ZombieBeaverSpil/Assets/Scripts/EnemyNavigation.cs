d8493a86a95d463bc52c2a26b391a8c654e2428a
Sundanian <thomas.krog.horne@gmail.com>
2015-10-30 10:44:46 +0100

    Enemy Navigation
    Added a test scene, with all the needed things included inside it.
    Except the enemyNavigation script, as this is needed in the game. This script is in the normal Scripts folder.
    
    Test scene can be deleted.
    
    Enemy navigation needs a nav mesh on the level.

---
+++ 3D-DATA-ZombieBeaverSpil/Assets/Scripts/EnemyNavigation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNavigation : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent myAgent;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    float distanceToPlayerBeforeHold;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = player.transform.position - transform.position;
+        if (Mathf.Sqrt(Mathf.Pow(v.x,2) + Mathf.Pow(v.y, 2) + Mathf.Pow(v.z, 2)) > distanceToPlayerBeforeHold)
+        {
+            myAgent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            myAgent.SetDestination(transform.position);
+        }
+    }
+}
---
+++ 3D-DATA-ZombieBeaverSpil/Assets/_Scenes/EnemyNavTestScene/PlayerNavigationScene.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavigationScene : MonoBehaviour
+{
+
+    /*
+    Dette script er udelukkende til eksempel scenen til enemy navigation
+        */
+
+    [SerializeField]
+    NavMeshAgent myAgent;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                myAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
