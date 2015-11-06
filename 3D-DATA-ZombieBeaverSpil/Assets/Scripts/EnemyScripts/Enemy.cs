fba2dc8752ae987ac632143779522a26aecc1d1c
Nicolai Bülov Nielsen <Nicolai3105@gmail.com>
2015-11-06 11:07:22 +0100

    Beaver movement sound

--- 3D-DATA-ZombieBeaverSpil/Assets/Scripts/EnemyScripts/Enemy.cs
+++ 3D-DATA-ZombieBeaverSpil/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -5,6 +5,8 @@ public class Enemy : MonoBehaviour
 {
     public AudioClip BeaverDie;
     public AudioClip BeaverHurt;
+    public AudioClip beaverStep;
+    public AudioClip beaverAttack;
     [SerializeField]
     NavMeshAgent myAgent;
     GameObject playerGO;
@@ -21,9 +23,15 @@ public class Enemy : MonoBehaviour
     private AudioSource source;
     private float volLowRange = 0.3f;
     private float volHighRange = 1.0f;
+    private float stepVolLowRange = 0.2f;
+    private float stepVolHighRange = 0.5f;
     private Animator myAnimator;
     private float deathTimer = 0;
     private bool dead;
+    float audioStepLengthWalk = 0.45f;
+    float timeBetweenSounds = 10.0f;
+    bool isPlaying = false;
+    int stepTimer = 0;
 
     // Use this for initialization
     void Awake()
@@ -39,6 +47,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        stepTimer++;
         playerGO = GameObject.FindGameObjectWithTag("Player");
         playerO = FindObjectOfType<Player>();
         if (!dead)
@@ -66,7 +75,6 @@ public class Enemy : MonoBehaviour
 
         if (health <= 0)
         {
-
             source.PlayOneShot(BeaverDie, vol);
             myAnimator.SetBool("Dying", true);
         }
@@ -80,6 +88,7 @@ public class Enemy : MonoBehaviour
             attackTime += Time.deltaTime;
             if (attackTime >= attackRate)
             {
+                source.PlayOneShot(beaverAttack);
                 (playerO as Player).TakeDamage(damage);
                 attackTime = 0;
                 myAnimator.SetBool("Attacking", true);
@@ -96,6 +105,20 @@ public class Enemy : MonoBehaviour
         Vector3 v = playerGO.transform.position - transform.position;
         if (Mathf.Sqrt(Mathf.Pow(v.x, 2) + Mathf.Pow(v.y, 2) + Mathf.Pow(v.z, 2)) > distanceToPlayerBeforeHold)
         {
+            float vol2 = Random.Range(stepVolLowRange, stepVolHighRange);
+            if (isPlaying == false)
+            {
+               source.PlayOneShot(beaverStep, vol2);
+               stepTimer++;
+               isPlaying = true;
+            }
+            if (stepTimer == 12)
+            {
+                stepTimer = 0;
+                isPlaying = false;
+            }
+            
+
             myAgent.SetDestination(playerGO.transform.position);
             myAnimator.SetBool("Walking", true);
         }
