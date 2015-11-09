1a0a9b1c768ab6d0f1c01bb80af16c484e997171
Poul Munk <pmunk87@gmail.com>
2015-11-09 13:59:24 +0100

    Swap weapon works. Health bar works.

---
+++ 3D-DATA-ZombieBeaverSpil/Assets/Scripts/UI/SwapRifleButton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SwapRifleButton : MonoBehaviour, IPointerDownHandler
+{
+    public Image uziButton, shotgunButton, handgunButton;
+
+    [SerializeField]
+    GameObject weapon;
+
+    private Vector3 inputVector;
+    private Image swapRifleButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        inputVector = Vector3.zero;
+        swapRifleButton = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        Vector2 pos;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(swapRifleButton.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / swapRifleButton.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / swapRifleButton.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            Debug.Log("SwapRifleButton was pressed.");
+            weapon.GetComponent<WeaponSwap>().currentWeapon = 3;
+
+            swapRifleButton.color = new Color32(0, 255, 55, 255);
+            uziButton.color = new Color32(0, 255, 55, 0);
+            shotgunButton.color = new Color32(0, 255, 55, 0);
+            handgunButton.color = new Color32(0, 255, 55, 0);
+
+        }
+    }
+}
---
+++ 3D-DATA-ZombieBeaverSpil/Assets/Scripts/UI/WeaponSwap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSwap : MonoBehaviour {
+
+    public int currentWeapon = 0;
+    private int maxWeapons = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SelectWeapon(currentWeapon);
+    }
+
+    public void SelectWeapon(int index)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (i == index)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
