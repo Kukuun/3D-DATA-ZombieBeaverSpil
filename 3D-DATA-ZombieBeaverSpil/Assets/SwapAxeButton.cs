507ede5c6b288a34270d6ecf9dcff605318c8bc3
Asbjørn Gjøderum-Svenningsen <Asbjorn1311@gmail.com>
2015-11-12 12:31:36 +0100

    More gameworld2 setup - testbuild2

--- 3D-DATA-ZombieBeaverSpil/Assets/SwapAxeButton.cs
+++ 3D-DATA-ZombieBeaverSpil/Assets/SwapAxeButton.cs
@@ -3,22 +3,22 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SwapHandgunButton : MonoBehaviour, IPointerDownHandler
+public class SwapAxeButton : MonoBehaviour, IPointerDownHandler
 {
-    public Image rifleButton, uziButton, shotgunButton, axeButton, sniperButton;
-    
+    public Image rifleButton, uziButton, shotgunButton, handgunButton, sniperButton;
+
     [SerializeField]
     GameObject weapon;
 
     private Vector3 inputVector;
-    private Image swapHandgunButton;
+    private Image swapAxeButton;
 
     // Use this for initialization
     void Start()
     {
         inputVector = Vector3.zero;
-        swapHandgunButton = GetComponent<Image>();
-        swapHandgunButton.color = new Color32(0, 255, 55, 255);
+        swapAxeButton = GetComponent<Image>();
+        swapAxeButton.color = new Color32(0, 255, 55, 255);
     }
 
     // Update is called once per frame
@@ -31,10 +31,10 @@ public class SwapHandgunButton : MonoBehaviour, IPointerDownHandler
     {
         Vector2 pos;
 
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(swapHandgunButton.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(swapAxeButton.rectTransform, ped.position, ped.pressEventCamera, out pos))
         {
-            pos.x = (pos.x / swapHandgunButton.rectTransform.sizeDelta.x);
-            pos.y = (pos.y / swapHandgunButton.rectTransform.sizeDelta.y);
+            pos.x = (pos.x / swapAxeButton.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / swapAxeButton.rectTransform.sizeDelta.y);
 
             inputVector = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
             inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
@@ -42,11 +42,11 @@ public class SwapHandgunButton : MonoBehaviour, IPointerDownHandler
             Debug.Log("SwapHandgunButton was pressed.");
             weapon.GetComponent<WeaponSwap>().currentWeapon = 0;
 
-            swapHandgunButton.color = new Color32(0, 255, 55, 255);
+            swapAxeButton.color = new Color32(0, 255, 55, 255);
             rifleButton.color = new Color32(0, 255, 55, 0);
             uziButton.color = new Color32(0, 255, 55, 0);
             shotgunButton.color = new Color32(0, 255, 55, 0);
-            axeButton.color = new Color32(0, 255, 55, 0);
+            handgunButton.color = new Color32(0, 255, 55, 0);
             sniperButton.color = new Color32(0, 255, 55, 0);
         }
     }
